4acb804f711114a0bb81a3e105f95473a7c4370b
Suresh-Master001 <suresh23112004@gmail.com>
2025-02-09 15:26:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function decryptMessage() {
     let text = document.getElementById("userMessage").value.toUpperCase();
     let decryptedText = text.split('').map(char => {
         let index = ciphertextAlphabet.indexOf(char);
-            return index !== -1 ? plaintextAlphabet[(index-3)%26] : char;
+            return index !== -1 ? plaintextAlphabet[(index-3+26)%26] : char;
     }).join('');
     if (prompt("Enter Password: ")=="chellam"){
         document.getElementById("gift-messages").classList.remove("hidden");
